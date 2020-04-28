d9db3c14c75aa828e414889ef368d26ba0320b3c
Chinmay <chinmaysharma45@gmail.com>
2020-04-28 13:59:45 +0530

    Updates

---
+++ precache-manifest.a6b597bbbbd5a696c80bbe273f96c942.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cab154d9d5574f67679a84bd5fbe644f",
+    "url": "/simple_calc_react/index.html"
+  },
+  {
+    "revision": "aa79220c367ad8645602",
+    "url": "/simple_calc_react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "47d8b69382051232c14d",
+    "url": "/simple_calc_react/static/js/2.d1beef8c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple_calc_react/static/js/2.d1beef8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa79220c367ad8645602",
+    "url": "/simple_calc_react/static/js/main.d44c16a2.chunk.js"
+  },
+  {
+    "revision": "357a920fd9a132197ab0",
+    "url": "/simple_calc_react/static/js/runtime-main.fef1f4d3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/simple_calc_react/static/media/logo.5d5d9eef.svg"
+  }
+]);
