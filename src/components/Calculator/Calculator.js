4fa9026e936b06e7b8d8c93f74a2fc50747224b1
Chinmay <chinmaysharma45@gmail.com>
2020-04-28 23:08:34 +0530

    feat: calculator skeleton v1

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import {Button,Container,Row,Col} from 'react-bootstrap';
 
+function add(num1,num2){
+    return num1 + num2;
+}
+
+function sub(num1,num2){
+    return num1 - num2;
+}
 
 function Calc() {
-    return <h2>Calc</h2>;
+    return(
+        <div>
+            <Container>
+                <Row>
+                    {/*DisplayField*/}
+                </Row>
+                
+            </Container>
+        </div>
+    );
   }
 
 export default Calc;
